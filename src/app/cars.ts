eb0e706ebc6d0b08bafe4b4290bc751cf6fa75a6
billyjack2 <billy.smith@corax.cloud>
2019-02-21 09:41:57 -0600

    Initial commit

---
+++ src/app/cars.ts
@@ -0,0 +1,70 @@
+
+
+export class Car {
+
+
+  constructor(public _id: number, private _name: string, private _fileId: string, private _classId: number){
+
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get name(){
+    return this._name
+  }
+
+  get fileId(){
+    return this._fileId
+  }
+
+  get classId(){
+    return this._classId
+  }
+
+
+
+
+}
+
+
+export class Cars{
+
+
+  private static list: Car[]  = new Array(0);
+
+
+  public static constructCars(){
+    Cars.list.push(new Car(100,"Porsche 919", "porsche919", 116));
+    Cars.list.push(new Car(39, "HPD ARX-01c", "hpdarx", 40));
+
+  }
+
+  public cars = (): Car[] =>  {
+    console.log(Cars.list.length);
+    return Cars.list;
+  }
+
+  public static cars1 = (): Car[] =>  {
+    console.log(Cars.list.length);
+    return Cars.list;
+  }
+
+  public static getCar = (id: number): Car => {
+    //return Cars.list.filter((car: Car) => car._id === id);
+    return Cars.list.find((car: Car) => car._id === id);
+  }
+
+}
+
+//console.log(new Cars().cars());
+Cars.constructCars();
+console.log(Cars.getCar(100).name);
+console.log(Cars.cars1());
+
+
+
+
+
+
